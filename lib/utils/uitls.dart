2619aad894b51a0b4dcfbff5fcb044924355eab1
Ajmianur Rahman <ajmianurrahman742@gmail.com>
2024-11-09 23:22:27 +0600

    Utils

---
+++ lib/utils/uitls.dart
@@ -0,0 +1,36 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+  static toastMessage(String message) {
+    Fluttertoast.showToast(msg: message);
+  }
+
+  static flushBarError(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        forwardAnimationCurve: Curves.decelerate,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(15),
+        backgroundColor: Colors.red.shade700,
+        reverseAnimationCurve: Curves.easeInOut,
+        message: message,
+        borderRadius: BorderRadius.circular(16),
+        positionOffset: 20,
+        icon: Icon(
+          Icons.error,
+          color: Colors.white,
+        ),
+        duration: Duration(seconds: 2),
+      )..show(context),
+    );
+  }
+
+  static snackBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(backgroundColor: Colors.black, content: Text(message)));
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_architecture/utils/routes/routes_name.dart';
+import 'package:mvvm_architecture/utils/uitls.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -17,12 +18,39 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           Center(
             child: InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.pushNamed(context, RoutesName.home);
               },
-              child: Text('Click'),
+              child: Text(
+                'Go To Home Screen',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          )
+          ),
+          SizedBox(height: 20,),
+          Center(
+            child: InkWell(
+              onTap: () {
+                Utils.toastMessage('This is the Toast');
+              },
+              child: Text(
+                'Show Toast Message',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Center(
+            child: InkWell(
+              onTap: () {
+                Utils.flushBarError('Flush bar message', context);
+              },
+              child: Text(
+                'Show Flush Bar Message',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
         ],
       ),
     );
